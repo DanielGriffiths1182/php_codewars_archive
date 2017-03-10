46145855fb60e5bee897c4517cc8d53e92486df8
Daniel Griffiths <logiconictvtube@gmail.com>
2017-03-10 09:56:29 -0500

    reorganized outer php tags and notes

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
-<!-- 1 "Volume of a Cuboid" - Give dimensions of cube, execute the proper operation to return volume. -->
 <?php
+// 1 "Volume of a Cuboid" - Give dimensions of cube, execute the proper operation to return volume.
   $kata = new class {
     public function get_volume_of_cuboid($length, $width, $height) {
       return $length * $width * $height;
     }
   };
-?>
 
-<!-- 2 "Keep Hydrated!" - Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
-     You get given the time and you need to return the number of litres Nathan will drink, rounded to the smallest value. -->
-<?php
+// 2 "Keep Hydrated!" - Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+// You get given the time and you need to return the number of litres Nathan will drink, rounded to the smallest value.
   function litres($t) {
     return floor($t / 2);
   }
