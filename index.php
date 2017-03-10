a13fbe9a1d981108247cb0077f234035abb4d9e5
Daniel Griffiths <logiconictvtube@gmail.com>
2017-03-10 09:43:27 -0500

    figured out how to activate the PHP code in atom <?php ?>

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <!-- 1 "Volume of a Cuboid" - Give dimensions of cube, execute the proper operation to return volume. -->
-$kata = new class {
-  public function get_volume_of_cuboid($length, $width, $height) {
-    return $length * $width * $height;
-  }
-};
+<?php
+  $kata = new class {
+    public function get_volume_of_cuboid($length, $width, $height) {
+      return $length * $width * $height;
+    }
+  };
+?>
