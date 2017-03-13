85ff47c18d25a5335b608862243ca3c891e4dc07
Daniel Griffiths <logiconictvtube@gmail.com>
2017-03-12 21:04:04 -0400

    PHP, 20, LVL 6, rectangles into squares, description, README

--- README.md
+++ README.md
@@ -75,3 +75,27 @@
  Your inputs: a positive integer, n, greater than or equal to one. n is provided, you have NO CONTROL over its value.
  Your expected outputs: an array of positive integers from 1 to n
  Your job is to write an algorithm that gets you from the input to the output.
+
+- 20 (LVL 6) "Rectangle into Squares" - The drawing below gives an idea of how to cut a given "true" rectangle into squares ("true"
+ rectangle meaning that the two dimensions are different).
+
+ Can you translate this drawing into an algorithm?
+
+ You will be given two dimensions
+
+ a positive integer length (parameter named lng)
+ a positive integer width (parameter named wdth)
+ You will return an array with the size of each of the squares.
+
+  sqInRect(5, 3) should return [3, 2, 1, 1]
+  sqInRect(3, 5) should return [3, 2, 1, 1]
+  
+ Note:
+
+ lng == wdth as a starting case would be an entirely different problem and the drawing is planned to be interpreted with lng !=
+ wdth. See kata, Square into Squares. Protect trees!.
+
+ When the initial parameters are so that lng == wdth, the solution [lng] would be the most obvious but not in the spirit of this
+ kata so, in that case, return None/nil/null/Nothing. Return {} with C++.
+
+ In that case the returned structure of C will have its sz component equal to 0. (See the "Solution" and "Examples" tabs)
--- index.php
+++ index.php
@@ -187,4 +187,41 @@ function pre_fizz($n) {
 function pre_fizz(int $n): array {
   return range(1, $n);
 }
+
+// 20 (LVL 6!!!) "Rectangle into Squares" - The drawing below gives an idea of how to cut a given
+// "true" rectangle into squares ("true" rectangle meaning that the two dimensions are different).
+
+// You will be given two dimensions
+//
+// a positive integer length (parameter named lng)
+// a positive integer width (parameter named wdth)
+// You will return an array with the size of each of the squares.
+//
+//   sqInRect(5, 3) should return [3, 2, 1, 1]
+//   sqInRect(3, 5) should return [3, 2, 1, 1]
+//   Note:
+//
+// lng == wdth as a starting case would be an entirely different problem and the drawing is planned to be
+// interpreted with lng != wdth. See kata, Square into Squares. Protect trees!.
+//
+// When the initial parameters are so that lng == wdth, the solution [lng] would be the most obvious but not in
+// the spirit of this kata so, in that case, return None/nil/null/Nothing. Return {} with C++.
+//
+// In that case the returned structure of C will have its sz component equal to 0. (See the "Solution" and "Examples" tabs)
+function sqInRect($lng, $wdth) {
+  $arr = [];
+    if ($lng === $wdth) {
+      return null;
+    }
+    while($lng > 0 && $wdth > 0) {
+      array_push($arr, min($lng, $wdth));
+      if ($lng >= $wdth) {
+        $lng -= $wdth;
+      }
+      else {
+        $wdth -= $lng;
+      }
+    }
+  return $arr;
+}
 ?>
